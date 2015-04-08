ccd3b7559ed1253d3a772eda1410cb123554389e
lillad25 <lillad25@yahoo.com>
2015-04-08 13:49:11 +0100

    implement paypal

--- routes.js
+++ routes.js
@@ -17,3 +17,30 @@ exports.init = function(c){
   config = c;
   paypal.configure(c.api);
 }
+
+exports.create = function(req, res){
+  var payment = {
+  "intent": "sale",
+  "payer": {
+    "payment_method": "paypal"
+  },
+  "redirect_urls": {
+    "return_url": "http://yoururl.com/execute",
+    "cancel_url": "http://yoururl.com/cancel"
+  },
+  "transactions": [{
+    "amount": {
+      "total": "5.00",
+      "currency": "USD"
+    },
+    "description": "My awesome payment"
+  }]
+};
+  paypal.payment.create(payment, function (error, payment) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(payment);
+    }   
+  });
+}
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express'),
 	routes = require('./routes'),
+	bodyParser = require('body-parser'),
+	morgan = require('morgan'),
+	session = require('express-session'),
 	http = require('http'),
 	path = require('path')
 	fs = require('fs'),
@@ -21,18 +24,38 @@ app.get('/', function(req, res){
 app.set('port', config.port || process.env.PORT);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
+// app.use(express.favicon());
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
+//setup the logger
+app.use(morgan('combined', {stream: accessLogStream}));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('you posted:\n');
+  res.end(JSON.stringify(req.body, null, 2));
+});
+// app.use(express.methodOverride());
+// app.use(express.cookieParser('your secret here'));
+// app.use(express.session());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+// app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
  
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-}
+// if ('development' === app.get('env')) {
+//   app.use(express.errorHandler());
+// }
  
 app.get('/', routes.index);
 app.get('/create', routes.create);
